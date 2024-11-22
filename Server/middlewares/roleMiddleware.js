a84bb25d8baac27335ce8f3aa51d5633ea62e46a
Ahmad Abo Rawi <aaborawi@uwo.ca>
2024-11-21 22:28:00 -0500

    Create user middlewares

---
+++ Server/middlewares/roleMiddleware.js
@@ -0,0 +1,11 @@
+
+function authorizeRole(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: 'Access denied: Admins only' });
+        }
+        next();
+    };
+};
+
+module.exports = authorizeRole;
