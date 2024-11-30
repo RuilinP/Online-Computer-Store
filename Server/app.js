5e93963cf52318cbcf4d2798934f166ea12001c2
Ahmad Abo Rawi <aaborawi@uwo.ca>
2024-11-29 22:13:23 -0500

    Centrilze the error handling, adding mout routes

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes'); // Define routes
 
+// Load environment variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
@@ -13,8 +14,11 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Server is running, welcome to the API' });
 });
 
-
+// Mount routes
 app.use('/api/users', userRoutes); // Mount user-related routes
+app.use('/api/computers', computerRoutes);
+app.use('/api/carts', cartRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Route not found' });
