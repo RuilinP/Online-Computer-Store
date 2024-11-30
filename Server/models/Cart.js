94fc7c44c88bf5a6044297220bd8c2a72c1153fb
Ahmad Abo Rawi <aaborawi@uwo.ca>
2024-11-29 21:50:37 -0500

    Create Cart model

---
+++ Server/models/Cart.js
@@ -0,0 +1,44 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/db'); //Import database connection
+const User = require('./User');
+
+class Cart extends Model { }
+
+Cart.init(
+    {
+        cart_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        // Create foreign keys:
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Cart',
+        tableName: 'carts',
+        timestamps: true,
+    }
+);
+
+
+//Relationship between carts and users
+User.hasMany(Cart, {
+    foreignKey: 'user_id',
+    as: 'cart'
+});
+
+Cart.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'user'
+});
+
+module.exports = Cart;
