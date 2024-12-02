39277435314d630b5220bfec0657f73c34e88965
Ahmad Abo Rawi <aaborawi@uwo.ca>
2024-12-02 17:40:17 -0500

    update aLL Server files

--- Server/app.js
+++ Server/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes'); // Define routes
+const computerRoutes = require('./routes/computerRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const sequelize = require('./config/db');
 
-// Load environment variables
+//Load env variales
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
 // Middleware 
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Server is running, welcome to the API' });
@@ -20,12 +26,10 @@ app.use('/api/computers', computerRoutes);
 app.use('/api/carts', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
-// Central error handling
+//Central error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
 });
-
-
 // Export the app instance
 module.exports = app;
