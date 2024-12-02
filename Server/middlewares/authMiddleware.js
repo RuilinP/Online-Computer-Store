48bfc6924c5826edda08e745600d65b49245eb22
Ahmad Abo Rawi <aaborawi@uwo.ca>
2024-12-02 17:16:15 -0500

    update auth middleware

--- Server/middlewares/authMiddleware.js
+++ Server/middlewares/authMiddleware.js
@@ -1,16 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
+    console.log('authenticateJWT middleware invoked');
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized: No token provided' });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = user;
+        console.log(`Authenticated user: ${JSON.stringify(req.user)}`);
         next();
     } catch (error) {
+        console.error('Authorization header missing.');
         res.status(401).json({ message: 'Unauthorized: Invalid token' });
     }
 };
